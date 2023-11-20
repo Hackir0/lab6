dc375edb73589c0e2a758912727313b0faa341fa
Zheki4 <zheka0812w@mai.ru>
2023-11-20 15:11:45 +0300

    немного исправил

--- Window.py
+++ Window.py
@@ -1,7 +1,7 @@
-from PyQt5 import  QtCore,QtGui, QtWidgets
-
-from db import DB
 import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from dbio import DB
+
 import os
 
 os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
@@ -10,11 +10,13 @@ os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
 class StartWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+
         self.result = QtWidgets.QLabel(self)
         self.result.setGeometry(250, 50, 200, 200)
+
         self.index = 0
         self.font = QtGui.QFont()
-        self.font.setPixelSize(12)
+        self.font.setPointSize(12)
 
         self.title = QtWidgets.QLabel(self)
         self.title.setGeometry(150, 100, 300, 25)
@@ -22,12 +24,12 @@ class StartWindow(QtWidgets.QMainWindow):
         self.font = QtGui.QFont()
         self.font.setPointSize(10)
         self.title.setFont(self.font)
-        self.title.setAlignment(QtCore.Qt.AlignCenter)
         self.title.setStyleSheet("color:black")
 
         self.setWindowTitle("Викторина")
         self.setFixedSize(600, 400)
         self.setStyleSheet("background-color:rgb(90,169,95)")
+
         self.startbutton = QtWidgets.QPushButton(self)
         self.startbutton.setGeometry(170, 280, 280, 20)
         self.startbutton.setFont(self.font)
@@ -36,10 +38,10 @@ class StartWindow(QtWidgets.QMainWindow):
         self.startbutton.clicked.connect(self.start_button)
 
         db = DB()
-        self.question = db.load_data()
+        self.questions = db.load_data()
 
         self.correct = 0
-        self.number = len(self.question)
+        self.number = len(self.questions)
 
         self.font = QtGui.QFont()
         self.font.setPointSize(12)
@@ -73,37 +75,36 @@ class StartWindow(QtWidgets.QMainWindow):
         self.pushButton.setFont(self.font)
         self.pushButton.setText("Следующий")
         self.pushButton.setStyleSheet("background-color:green")
-        self.pushButton.clicked.connect(self.show_question)
+        self.pushButton.clicked.connect(self.show_questions)
 
         self.pushButton.hide()
 
-    def show_question(self):
+    def show_questions(self):
 
-        if self.index < len(self.question):
+        if self.index < len(self.questions):
             self.questions[self.index]['answer'] = self.line.text().lower()
             print(self.questions[self.index]['correct_answer'], self.line.text().lower())
 
-            if self.question[self.index]['correct_answer'] == self.line.text().lower():
+            if self.questions[self.index]['correct_answer'] == self.line.text().lower():
                 self.correct += 1
-
-            self.line.close()
+            self.line.clear()
 
             self.index += 1
 
-            if self.index < len(self.question):
-                self.question_text.setText(self.question[self.index]['question'])
+            if self.index < len(self.questions):
+                self.question_text.setText(self.questions[self.index]['question'])
             else:
                 self.line.hide()
                 self.pushButton.hide()
                 self.question_text.hide()
-
                 print(self.correct, self.number)
-                self.result.setText(f"Ваш результат:{(self.correct / self.number) * 100}%")
+                self.result.setText(f"Ваш результат: {(self.correct / self.number) * 100} %")
+                self.result.setWordWrap(True)
 
     def start_button(self):
         self.startbutton.hide()
         self.question_text.show()
         self.line.show()
         self.pushButton.show()
-        self.question_text.setText(self.question[0]['question'])
+        self.question_text.setText(self.questions[0]['question'])
         self.title.hide()
---
+++ dbio.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+
+class DB:
+    def __init__(self):
+        self.conn = sqlite3.connect('question.db')
+
+    def load_data(self):
+        request = "SELECT question, answer FROM question"
+        info = self.conn.execute(request).fetchall()
+        data = []
+        for i in range(len(info)):
+            data.append({'id': i, 'question': info[i][0], 'correct_answer': info[i][1], 'answer': '', 'is_showed': 0})
+        print(data)
+        return data
+
+
+db = DB()
+
+db.load_data()
